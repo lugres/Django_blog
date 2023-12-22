1bfc0d48f7c5f90c4dc7a86633226b4755af0cf4
Lugres <lugres@gmail.com>
2023-12-22 10:47:55 -0500

    Add templates and some dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# Some dummy post data for testing purpose
+posts = [
+	{
+		'author': 'Angela Lu',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'December 11, 2022'
+	},
+	{
+		'author': 'Jimmy Chin',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'December 15, 2022'
+	},
+]
 
 
-def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+def  home(request):
+	context = {
+		'posts': posts
+	}
+
+	return render(request, 'blog/home.html', context)
 
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html')
