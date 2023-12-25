fe8b430c3d02d1f385fff16a04859fc86740645a
Lugres <lugres@gmail.com>
2023-12-25 11:47:22 -0500

    Add Delete view so users can delete their posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 # Not used anymore, kept for demostartive purposes - compare with the next class-based view
@@ -44,6 +44,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 		return super().form_valid(form)
 
 	# will be run by UserPassesTestMixin
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Post
+	success_url = '/'
+
 	def test_func(self):
 		post = self.get_object()
 		if self.request.user == post.author:
